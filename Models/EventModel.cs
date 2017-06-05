e27c000de1895e8e09928b050a391fd39959225c
kevinburgess <kevin.burgess@gmail.com>
2017-06-05 10:07:39 -0400

    Adding all files except for Kendo references

---
+++ Models/EventModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TextMe.Models
+{
+    public class EventModel
+    {
+        public int ID { get; set; }
+        public string Action { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+        private DateTime time;
+        public DateTime Time
+        {
+            get
+            {
+                return time;
+            }
+
+            set
+            {
+                time = value.ToUniversalTime();
+            }
+        }
+        public string Phone { get; set; }
+    }
+}
